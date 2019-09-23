371ded993eaf31ca7e4ffdd5d1456de26c5a6b0b
xkinepa <xkinepa@gmail.com>
2019-09-23 18:07:38 +0800

    add tests

--- Alemow.Tests/AutofacTests.cs
+++ Alemow.Tests/AutofacTests.cs
@@ -1,12 +1,39 @@
-﻿namespace Alemow
+﻿using Alemow.Autofac;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Alemow
 {
     public class AutofacTests : AutofacTestsBase
     {
         public AutofacTests() : base(b =>
         {
-            ;
+            b.RegisterInstance(new ConfigurationBuilder().Build()).As<IConfiguration>();
+            b.AutoRegisterAssembly(typeof(AutofacTests).Assembly);
         })
         {
+        }
+
+        [Fact]
+        public void Component()
+        {
+            Container.IsRegistered(typeof(A));
+            Container.IsRegistered(typeof(IA));
+            Container.IsRegisteredWithKey("A", typeof(IA));
+
+            IA a1, a2;
+            using (var context = Container.BeginLifetimeScope())
+            {
+                a1 = context.Resolve<IA>();
+            }
+
+            using (var context = Container.BeginLifetimeScope())
+            {
+                a2 = context.Resolve<IA>();
+            }
+
+            Assert.Same(a1, a2);
         }
     }
 }
---
+++ Alemow.Tests/Beans.cs
@@ -0,0 +1,54 @@
+﻿using Alemow.Attributes;
+using Autofac;
+
+namespace Alemow
+{
+    [Configuration]
+    public class BModule : Module
+    {
+        [Bean(false, false),
+         Scope(Scope.PerDependency),
+         As(typeof(IB)),
+         Key("B", typeof(IB))]
+        public IB DefineB([ConfigValue("foo")] string foo, [Inject] C c)
+        {
+            return new B();
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<C>();
+        }
+    }
+
+    [Component(true, false),
+     Scope(Scope.Singleton),
+     As(typeof(IA)),
+     Key("A", typeof(IA))]
+    public class A : IA
+    {
+        [ConfigValue("foo")] private readonly string _foo;
+        [Inject("B")] private readonly IB _b;
+
+        public string Foo => _foo;
+        public IB B => _b;
+    }
+
+    public interface IA
+    {
+        string Foo { get; }
+        IB B { get; }
+    }
+
+    public class B : IB
+    {
+    }
+
+    public interface IB
+    {
+    }
+
+    public class C
+    {
+    }
+}
